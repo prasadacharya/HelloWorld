b0ae945c7cb3d34b0583a046eb298bad8ecfafb9
Vibha <vibhaprabhu10@gmail.com>
2020-01-22 11:03:59 -0500

    version 3

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("From Windows!");
+            String message = Console.ReadLine();
         }
     }
 }
